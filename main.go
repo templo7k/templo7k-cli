4b2afadb555cb28ff0f4cfe3376b9dc0dc97f60d
Gustav L <h5d3s@protonmail.com>
2021-12-14 13:46:57 -0300

    feat: improving a little bit the source code

--- main.go
+++ main.go
@@ -7,58 +7,51 @@ import (
 	"os"
 )
 
-func error(msg string) string {
-	fmt.Fprintf(os.Stderr, "An error has occured: %s", msg)
-	return "err"
+var opt int
+
+func download_pdf(post_name string) error {
+	paper, err := http.Get("https://github.com/templo7k/templo7k-cli/raw/main/papers/" + post_name + ".pdf")
+
+	if paper.StatusCode != http.StatusOK {
+		fmt.Println("Bad status code")
+	} else if paper.StatusCode == 200 {
+		fmt.Println("PDF Downloaded")
+	} else {
+		fmt.Println("Paper does not exist (input only numbers)")
+	}
+
+	defer paper.Body.Close()
+
+	out, err := os.Create(post_name + ".pdf")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, paper.Body)
+	return err
 }
 
-func main() {
+func verify_option(option int) string {
+	valid_options := map[int]string{
+		1: "AAAA",
+		2: "BBBB",
+	}
+
+	return valid_options[option]
+}
+
+func choose_option() {
 	fmt.Println("Welcome to the CLI version of templo7k.ninja!\n\nChoose the article you will read and we will automatically save it's pdf to your machine.")
 	fmt.Println("1 - Programming\n2 - Cyber Security\n3 - Mathematics/Physics\n4 - Others\nInput option below (1-4) ")
 
-	var topic uint8
-	var opt int
-
-	fmt.Scanln(&topic)
-
-	switch topic {
-	case 1:
-		fmt.Println("\nProgramming\n1 - Bit Shift Operators")
-		fmt.Scanln(&opt)
-		defer programming(opt)
-	case 2:
-		fmt.Println("No paper yet")
-	case 3:
-		fmt.Println("No paper yet")
-	case 4:
-		fmt.Println("No paper yet")
-	default:
-		error("An error occured, input only a number between 1-4")
-	}
-
+	fmt.Scanln(&opt)
 }
 
-func programming(prog int) int {
-	if prog == 1 {
-		out, err := os.Create("bit_shift.pdf")
-		paper, err := http.Get("https://github.com/templo7k/templo7k-cli/raw/main/papers/bit_shift.pdf")
-
-		defer out.Close()
-
-		defer paper.Body.Close()
-
-		_, err = io.Copy(out, paper.Body)
-
-		fmt.Println("PDF Downloaded")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-		if paper.StatusCode != http.StatusOK {
-			error("Bad status code")
-		}
-	} else {
-		error("Paper does not exist (input only numbers)")
-	}
-	return 0
+func main() {
+	choose_option()
+	chosed_option_verify := verify_option(opt)
+	fmt.Println(chosed_option_verify)
 }
